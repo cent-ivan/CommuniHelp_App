1bdfc888575ffd3de6b9d78f12f1d48dff68bead
Vincent_Ivan <vpalomata25@gmail.com>
2024-06-27 23:30:53 +0800

    created own directory

---
+++ lib/Views/Pages/Dashboard_Page/Dashboard_components/dashboard_buttons.dart
@@ -0,0 +1,211 @@
+//Infographics Section
+import 'package:flutter/material.dart';
+
+class InfographicsSection extends StatelessWidget {
+  const InfographicsSection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 180,
+      padding: const EdgeInsets.all(10),
+      margin: const EdgeInsets.fromLTRB(9, 0, 9, 12),
+      decoration: const BoxDecoration(
+        color:  Color(0x8CA4EACD),
+        borderRadius: BorderRadius.all(Radius.circular(18 ))
+      ),
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            margin: const EdgeInsets.fromLTRB(9, 3, 9, 5),
+            child: const Text(
+              "Infographics",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF3D424A), 
+              ),
+            ),
+          ),
+      
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                MaterialButton(
+                  onPressed: () {},
+                  height: 100,
+                  minWidth: 150,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(15))
+                  ),
+                  color: Colors.white,
+                  child: const Icon(
+                    Icons.accessibility_sharp
+                  ),
+                ),
+    
+                const SizedBox(width: 10,),
+            
+                MaterialButton(
+                  onPressed: () {},
+                  height: 100,
+                  minWidth: 150,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(15))
+                  ),
+                  color: Colors.white,
+                  child: const Icon(
+                    Icons.accessibility_sharp
+                  ),
+                ),
+            
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}//Inforgraphic Section
+
+
+
+//Utiltity Section
+class UtilitySection extends StatelessWidget {
+  const UtilitySection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 250,
+      padding: const EdgeInsets.all(10),
+      margin: const EdgeInsets.fromLTRB(9, 5, 9, 9),
+      decoration: const BoxDecoration(
+        color:  Color(0x4D57BEE6),
+        borderRadius: BorderRadius.all(Radius.circular(18 ))
+      ),
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            margin: const EdgeInsets.fromLTRB(9, 3, 9, 5),
+            child: const Text(
+              "Utility",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF3D424A), 
+              ),
+            ),
+          ),
+
+          const UtilityButtons()
+
+        ],
+      ),
+    );
+  }
+}
+
+
+//Utility Buttons
+class UtilityButtons extends StatelessWidget {
+  const UtilityButtons({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      spacing: 5,
+      runSpacing: 15,
+      crossAxisAlignment: WrapCrossAlignment.center,
+      children: [
+        MaterialButton(
+          onPressed: () {},
+          height: 80,
+          minWidth: 100,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(25))
+          ),
+          color: Colors.white,
+          child: const Icon(
+            Icons.accessibility_sharp
+          ),
+        ),
+    
+        MaterialButton(
+          onPressed: () {},
+          height: 80,
+          minWidth: 100,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(25))
+          ),
+          color: Colors.white,
+          child: const Icon(
+            Icons.accessibility_sharp
+          ),
+        ),
+    
+        MaterialButton(
+          onPressed: () {},
+          height: 80,
+          minWidth: 100,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(25))
+          ),
+          color: Colors.white,
+          child: const Icon(
+            Icons.accessibility_sharp
+          ),
+        ),
+    
+        MaterialButton(
+          onPressed: () {},
+          height: 80,
+          minWidth: 100,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(25))
+          ),
+          color: Colors.white,
+          child: const Icon(
+            Icons.accessibility_sharp
+          ),
+        ),
+    
+        MaterialButton(
+          onPressed: () {},
+          height: 80,
+          minWidth: 100,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(25))
+          ),
+          color: Colors.white,
+          child: const Icon(
+            Icons.accessibility_sharp
+          ),
+        ),
+    
+        MaterialButton(
+          onPressed: () {},
+          height: 80,
+          minWidth: 100,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(25))
+          ),
+          color: Colors.white,
+          child: const Icon(
+            Icons.accessibility_sharp
+          ),
+        ),
+    
+      ],
+    );
+  }
+}//Utiltity Section
---
+++ lib/Views/Pages/Dashboard_Page/dashboard_view.dart
@@ -0,0 +1,120 @@
+import 'package:communihelp_app/Views/Pages/Dashboard_Page/Dashboard_components/dashboard_buttons.dart';
+import 'package:flutter/material.dart';
+
+class DashboardView extends StatefulWidget {
+  const DashboardView({super.key});
+
+  @override
+  State<DashboardView> createState() => _DashboardViewState();
+}
+
+class _DashboardViewState extends State<DashboardView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white54,//const Color(0xFFE0E0E0),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(13),
+          height: 750,
+          child:  Column(
+            children: <Widget>[
+
+              //ANNOUNCEMENT SECTION
+              const AnnouncementSection(),
+
+              Container(
+                alignment: Alignment.topLeft,
+                margin: const EdgeInsets.fromLTRB(9, 12, 9, 9),
+                child: const Text(
+                  "DASHBOARD",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF3D424A), 
+                  ),
+                ),
+              ),
+
+              //INFOGRAPHICS SECTION
+              const InfographicsSection(),
+
+              //UTILITY SECTION
+              const UtilitySection()
+
+
+            ],
+          )
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+//-----------------------------------------------------------------------------------------------
+//Announcement Section
+class AnnouncementSection extends StatefulWidget {
+  const AnnouncementSection({
+    super.key,
+  });
+
+  @override
+  State<AnnouncementSection> createState() => _AnnouncementSectionState();
+}
+
+class _AnnouncementSectionState extends State<AnnouncementSection> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+    
+        //Title
+        Container(
+          alignment: Alignment.topLeft,
+          margin: const EdgeInsets.fromLTRB(9, 3, 9, 0),
+          child: const Text(
+            "Announcement", 
+              style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold, 
+              color: Color(0xFF3D424A),               
+            ),
+          ), 
+        ),
+        
+        //Announcement Tiles
+        SizedBox(
+          width: 350,
+          height: 150,
+          child: ListView.builder(
+            itemCount: 6,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(8),
+                child: Container(
+                  width: 280,
+                  height: 160,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(9)),
+                    gradient: LinearGradient(
+                      colors: [Color(0xCCFEAE49), Color(0x80FEC57C), Colors.white70, Colors.white60, ], 
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      tileMode: TileMode.decal
+                    )
+                  ),
+                ),
+              );
+            }
+          ),
+        ),
+      ],
+      
+    );
+  }
+}
