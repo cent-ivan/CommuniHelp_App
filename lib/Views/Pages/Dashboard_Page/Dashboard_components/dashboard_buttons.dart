b0cf753d272d79a894a21f20fe94c1b150160532
Vincent_Ivan <vpalomata25@gmail.com>
2024-06-30 12:54:52 +0800

    seperated utility button from dashboard buttons

--- lib/Views/Pages/Dashboard_Page/Dashboard_components/dashboard_buttons.dart
+++ lib/Views/Pages/Dashboard_Page/Dashboard_components/dashboard_buttons.dart
@@ -1,4 +1,6 @@
 //Infographics Section
+import 'package:communihelp_app/Views/Pages/Dashboard_Page/dashboard_components/dashboard_buttons.dart';
+import 'package:communihelp_app/Views/Pages/Dashboard_Page/dashboard_components/utility_buttons.dart';
 import 'package:flutter/material.dart';
 
 class InfographicsSection extends StatelessWidget {
@@ -146,99 +148,3 @@ class UtilitySection extends StatelessWidget {
   }
 }
 
-
-//Utility Buttons
-class UtilityButtons extends StatelessWidget {
-  const UtilityButtons({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Wrap(
-      spacing: 5,
-      runSpacing: 15,
-      crossAxisAlignment: WrapCrossAlignment.center,
-      children: [
-        MaterialButton(
-          onPressed: () {},
-          height: 80,
-          minWidth: 100,
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(25))
-          ),
-          color: Colors.white,
-          child: const Icon(
-            Icons.accessibility_sharp
-          ),
-        ),
-    
-        MaterialButton(
-          onPressed: () {},
-          height: 80,
-          minWidth: 100,
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(25))
-          ),
-          color: Colors.white,
-          child: const Icon(
-            Icons.accessibility_sharp
-          ),
-        ),
-    
-        MaterialButton(
-          onPressed: () {},
-          height: 80,
-          minWidth: 100,
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(25))
-          ),
-          color: Colors.white,
-          child: const Icon(
-            Icons.accessibility_sharp
-          ),
-        ),
-    
-        MaterialButton(
-          onPressed: () {},
-          height: 80,
-          minWidth: 100,
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(25))
-          ),
-          color: Colors.white,
-          child: const Icon(
-            Icons.accessibility_sharp
-          ),
-        ),
-    
-        MaterialButton(
-          onPressed: () {},
-          height: 80,
-          minWidth: 100,
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(25))
-          ),
-          color: Colors.white,
-          child: const Icon(
-            Icons.accessibility_sharp
-          ),
-        ),
-    
-        MaterialButton(
-          onPressed: () {},
-          height: 80,
-          minWidth: 100,
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(25))
-          ),
-          color: Colors.white,
-          child: const Icon(
-            Icons.accessibility_sharp
-          ),
-        ),
-    
-      ],
-    );
-  }
-}//Utiltity Section
