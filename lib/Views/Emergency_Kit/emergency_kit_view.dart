9722dca665479d662228028b8d95c3be4d0f3f2c
Vincent_Ivan <vpalomata25@gmail.com>
2024-07-02 23:39:41 +0800

    added emergency outline

---
+++ lib/Views/Emergency_Kit/emergency_kit_view.dart
@@ -0,0 +1,21 @@
+//to make the text color of the page adapt to dark mode enter this `Theme.of(context).colorScheme.outline`
+//for the contact tiles here is the color: `Theme.of(context).colorScheme.primary`
+//if you find the code to be difficult jsut search in youtube `To Do App in flutter`
+
+//see the design in the figma link
+//NOTE: pre-load the items stated in the design in the list
+import 'package:flutter/material.dart';
+
+class EmergencyKitView extends StatefulWidget {
+  const EmergencyKitView({super.key});
+
+  @override
+  State<EmergencyKitView> createState() => _EmergencyKitViewState();
+}
+
+class _EmergencyKitViewState extends State<EmergencyKitView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
