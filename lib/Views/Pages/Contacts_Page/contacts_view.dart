6b7687b8d81bcc2128142286bb1ea43d0402da13
Vincent_Ivan <vpalomata25@gmail.com>
2024-07-02 23:29:22 +0800

    added contacts_view outline

--- lib/Views/Pages/Contacts_Page/contacts_view.dart
+++ lib/Views/Pages/Contacts_Page/contacts_view.dart
@@ -7,13 +7,24 @@ class ContactsView extends StatefulWidget {
   State<ContactsView> createState() => _ContactsViewState();
 }
 
+
+//to make the text color of the page adapt to dark mode enter this `Theme.of(context).colorScheme.outline`
+//for the contact tiles here is the color: `Theme.of(context).colorScheme.primary`
+//if you find the code to be difficult jsut search in youtube `contacts app in flutter`
 class _ContactsViewState extends State<ContactsView> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Contacts"),
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(13),
+        child: Container(
+          child: const Text("Enter your code here:"), //remove the text widget
+          
+        ),
       ),
+
+
     );
   }
 }
