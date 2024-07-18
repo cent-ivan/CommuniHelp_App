6323336f9f38de7740feca9b9477118c74832b41
Vincent_Ivan <vpalomata25@gmail.com>
2024-07-18 14:43:00 +0800

    added clamping scroll physics

--- lib/Views/Bottom_App_Bar_Pages/Dashboard_Page/dashboard_view.dart
+++ lib/Views/Bottom_App_Bar_Pages/Dashboard_Page/dashboard_view.dart
@@ -1,3 +1,4 @@
+import 'package:communihelp_app/Views/Bottom_App_Bar_Pages/Dashboard_Page/dashboard_components/announcement_section.dart';
 import 'package:communihelp_app/Views/Bottom_App_Bar_Pages/Dashboard_Page/dashboard_components/dashboard_buttons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -15,6 +16,7 @@ class _DashboardViewState extends State<DashboardView> {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,//if dark mode const Color(0xFF3D424A)
       body: SingleChildScrollView(
+        physics: const ClampingScrollPhysics(),
         child: Container(
           padding: const EdgeInsets.fromLTRB(9, 15, 9, 0).r,
           height: 1075.r,
@@ -38,7 +40,7 @@ class _DashboardViewState extends State<DashboardView> {
               ),
 
               //INFOGRAPHICS SECTION
-              const InfographicsSection(),
+              const InfographicsSection(), //Dashboard buttons
 
               //UTILITY SECTION
               const UtilitySection()
@@ -55,100 +57,3 @@ class _DashboardViewState extends State<DashboardView> {
 
 
 
-
-//-----------------------------------------------------------------------------------------------
-//Announcement Section
-class AnnouncementSection extends StatefulWidget {
-  const AnnouncementSection({
-    super.key,
-  });
-
-  @override
-  State<AnnouncementSection> createState() => _AnnouncementSectionState();
-}
-
-class _AnnouncementSectionState extends State<AnnouncementSection> {
-  final int numOfAnnouncement = 2; //get total announcement in database
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-    
-        //Title
-        Container(
-          alignment: Alignment.topLeft,
-          margin: const EdgeInsets.fromLTRB(9, 3, 9, 0).r,
-          child: Text(
-            "Announcement", 
-              style: TextStyle(
-              fontSize: 25.r,
-              fontWeight: FontWeight.bold, 
-              color: Theme.of(context).colorScheme.outline,               
-            ),
-          ), 
-        ),
-        
-        //Announcement Tiles
-        SizedBox(
-          width: 350.r,
-          height: 150.r,
-          child: ListView.builder(
-            itemCount: numOfAnnouncement,
-            scrollDirection: Axis.horizontal,
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.all(8).r,
-                child: Container(
-                  width: 280.r,
-                  height: 160.r,
-                  padding: EdgeInsets.all(8).r,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(9).r),
-                    gradient: LinearGradient(
-                      colors: [const Color(0xCCFEAE49), const Color(0x80FEC57C), Theme.of(context).colorScheme.surface, ], 
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      tileMode: TileMode.decal
-                    ),
-                  ),
-
-                  //TODO: Change dummy text
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        width: 155.r,
-                        margin: const EdgeInsets.only(bottom: 8).r,
-                        decoration: BoxDecoration(
-                          color: Colors.black26,
-                          borderRadius: BorderRadius.all(Radius.circular(5).r)
-                        ),
-                        child: Text(
-                          "Nabas Announcement",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 12.r
-                          ),
-                        ),
-                      ),
-
-                      Text(
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec imperdiet sit amet leo sed tempor.",
-                        style: TextStyle(
-                          fontSize: 11.r
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            }
-          ),
-        ),
-      ],
-      
-    );
-  }
-}
