18ac6fc1fda162f8d163f5148e739c1c3d525aa7
Vincent_Ivan <vpalomata25@gmail.com>
2024-07-18 14:41:16 +0800

    separated announcement

---
+++ lib/Views/Bottom_App_Bar_Pages/Dashboard_Page/dashboard_components/announcement_section.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+//-----------------------------------------------------------------------------------------------
+//Announcement Section
+
+class AnnouncementSection extends StatefulWidget {
+  const AnnouncementSection({
+    super.key,
+  });
+
+  @override
+  State<AnnouncementSection> createState() => _AnnouncementSectionState();
+}
+
+class _AnnouncementSectionState extends State<AnnouncementSection> {
+  final int numOfAnnouncement = 2; //get total announcement in database
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+    
+        //Title
+        Container(
+          alignment: Alignment.topLeft,
+          margin: const EdgeInsets.fromLTRB(9, 3, 9, 0).r,
+          child: Text(
+            "Announcement", 
+              style: TextStyle(
+              fontSize: 25.r,
+              fontWeight: FontWeight.bold, 
+              color: Theme.of(context).colorScheme.outline,               
+            ),
+          ), 
+        ),
+        
+        //Announcement Tiles
+        SizedBox(
+          width: 350.r,
+          height: 150.r,
+          child: ListView.builder(
+            itemCount: numOfAnnouncement,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(8).r,
+                child: Container(
+                  width: 280.r,
+                  height: 160.r,
+                  padding: const EdgeInsets.all(8).r,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(const Radius.circular(9).r),
+                    gradient: LinearGradient(
+                      colors: [const Color(0xCCFEAE49), const Color(0x80FEC57C), Theme.of(context).colorScheme.surface, ], 
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      tileMode: TileMode.decal
+                    ),
+                  ),
+
+                  //TODO: Change dummy text
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: 155.r,
+                        margin: const EdgeInsets.only(bottom: 8).r,
+                        decoration: BoxDecoration(
+                          color: Colors.black26,
+                          borderRadius: BorderRadius.all(const Radius.circular(5).r)
+                        ),
+                        child: Text(
+                          "Nabas Announcement",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12.r
+                          ),
+                        ),
+                      ),
+
+                      Text(
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec imperdiet sit amet leo sed tempor.",
+                        style: TextStyle(
+                          fontSize: 11.r
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            }
+          ),
+        ),
+      ],
+      
+    );
+  }
+}
