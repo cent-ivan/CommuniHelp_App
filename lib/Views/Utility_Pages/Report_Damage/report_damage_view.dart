29c3ed680b66bccf38479a4998f62c923f0ca68c
Vincent_Ivan <vpalomata25@gmail.com>
2024-08-21 21:18:55 +0800

    added firebase configs and files

---
+++ lib/Views/Utility_Pages/Report_Damage/report_damage_view.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ReportDamageView extends StatefulWidget {
+  const ReportDamageView({super.key});
+
+  @override
+  State<ReportDamageView> createState() => _ReportDamageViewState();
+}
+
+class _ReportDamageViewState extends State<ReportDamageView> {
+  @override
+  Widget build(BuildContext context) {
+    return PopScope(
+      canPop: false,
+      child: Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.surface,
+      
+        appBar: const ReportAppBar(),
+      
+        //Tab contents, entre code here
+        body: Container()
+      ),
+    );
+  }
+}
+
+
+
+//APP BAR----------------------------------------------------------------------------------------------------
+class ReportAppBar extends StatelessWidget implements PreferredSizeWidget{
+  const ReportAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      elevation: 1,
+      title: ShaderMask(
+        shaderCallback: (bounds) => LinearGradient(
+          colors: [Theme.of(context).colorScheme.outline, const Color(0x80FEAE49),   const Color(0xFF57BEE6)],
+        ).createShader(bounds),
+          
+        child: const Text(
+          "Report To Authorities",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+          
+      leading: IconButton(
+        icon: const Icon(Icons.arrow_back_ios_new),
+        iconSize: 20,
+        onPressed: () {
+          Navigator.pushNamedAndRemoveUntil(context, '/home', (Route<dynamic> route) => false);
+        },
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight.r);
+}
