6fa1d13d276afc97a8b6efef33e35d6039ca4abb
zamPRIME <casseyzam@gmail.com>
2024-08-21 16:42:44 +0800

    added zam's checklist

---
+++ lib/Views/Utility_Pages/Report_Damage/report_damage_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ReportDamageView extends StatelessWidget {
+  const ReportDamageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
