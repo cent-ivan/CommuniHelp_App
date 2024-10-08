0e61ee1c6bce0799ea8c7f3b4b1182ab2719830e
Vincent_Ivan <vpalomata25@gmail.com>
2024-10-08 19:30:49 +0800

    added Hive init

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:get/get.dart';
 
@@ -37,6 +38,7 @@ void main() async{
     DeviceOrientation.portraitDown
   ]);
 
+  await Hive.initFlutter();
   
   runApp(
     MultiProvider(
