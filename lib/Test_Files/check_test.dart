392f720b173a2a15d4dd247e0c95b8e95c007ebe
Vincent_Ivan <vpalomata25@gmail.com>
2024-10-16 08:40:38 +0800

    added unit test class

---
+++ lib/Test_Files/check_test.dart
@@ -0,0 +1,12 @@
+class CheckTest {
+  //final emergencyViewModel = EmergencyViewModel();
+
+
+  void checkCGList(List cgList) {
+    print("Coast Guard List ${cgList.length}");
+  }
+
+  void displayCalled(String methodName) {
+    print(">>> $methodName called...");
+  } 
+}
